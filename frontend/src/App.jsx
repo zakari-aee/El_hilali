07e86a4165a96a19c22d76d9cecad2aa240dc082
zakari-aee <zakariaealliouate@gmail.com>
2025-12-17 12:42:31 +0100

    backend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-900">
+      <h1 className="text-4xl font-extrabold text-orange-500">
+        Tailwind v3 + React + Vite ✅
+      </h1>
+    </div>
+  )
+}
