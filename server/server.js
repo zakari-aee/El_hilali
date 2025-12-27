8152b42325793b6a7bbb5c05806c4210bdcfe797
zakari-aee <zakariaealliouate@gmail.com>
2025-12-27 15:03:30 +0100

    .

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 5000;
 
 // ==================== CORS Configuration ====================
 const corsOptions = {
-  origin: ['http://localhost:5173', 'http://localhost:3000', 'http://localhost:5000'],
+  origin: ['http://localhost:5173', 'http://localhost:3000', 'http://localhost:5000', 'http://192.168.11.106:5173'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
